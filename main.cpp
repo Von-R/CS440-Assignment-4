68c0dc4071c8a2c95715851650cfce47255c9c93
Von Davis <davisdae@oregonstate.edu>
2024-02-26 18:35:10 -0800

    cleaned up

--- main.cpp
+++ main.cpp
@@ -1,31 +1,26 @@
 #include <bits/stdc++.h>
 #include "record_class.h"
-
 using namespace std;
 
-#define BUFFER_SIZE 10
-//Records buffers[BUFFER_SIZE];
+#define BUFFER_SIZE 21
+
 vector<Records> buffers(BUFFER_SIZE);
 vector<string> tempFiles;
 
+// Used in priority queue to sort records
 struct CompareRecords {
     bool operator()(const Records& a, const Records& b) const {
-        return a.emp_record.eid > b.emp_record.eid; // Assuming you want the smallest eid to have the highest priority
+        return a.emp_record.eid > b.emp_record.eid;
     }
 };
 
-
-// Utility function to load records, sort them, and write to temp files
+// Load records, sort them, and write to temp files
 // Results in all records being written to temp files: first run
-int Sort_Buffer(fstream &empin) {
+void Sort_Buffer(fstream &empin) {
     string runFileName;
     Records tempRecord;
     int runs = 0;
     int counter = 0;
-    bool eofFlag = false;
-
-    // Load records into the buffer
-    //tempRecord.printRecord();
     
     // Loop gets records from Emp.csv and loads them into the page buffer: loads a single record
     // Terminates when main memory is full or no more records to load
@@ -36,41 +31,31 @@ int Sort_Buffer(fstream &empin) {
     // 3. Write sorted buffer to temporary file
     // 4. Clear buffer
     // 5. Repeat until no more records to load
+
+    // Terminate loop when no more records to load
     while (tempRecord.no_values != -1) {
 
         // Load records into the buffer
         while (counter < BUFFER_SIZE && tempRecord.no_values != -1) {
             tempRecord = Grab_Emp_Record(empin);
             if (tempRecord.no_values == -1) {
-                cout << "No more records to load..." << endl;
-                cout << "End of Sort_Buffer function... \nRuns: " << runs << endl << endl;
-                return runs;
+                return;
                 }
             
-            //tempRecord.printRecord();
+            // Load record into buffer
             buffers.at(counter++) = tempRecord;
         }
 
         // Sort the buffer
-        cout << "Sorting buffer..." << endl;
         std::sort(buffers.begin(), buffers.end(), [](const Records& a, const Records& b) {
             return a.emp_record.eid < b.emp_record.eid;
         });
 
-        // Print out contents of buffer by eid
-        vector<Records>::iterator it = buffers.begin();
-        cout << "Printing sorted buffer..." << endl;
-        while (it != buffers.end() && it->no_values != -1) {
-            cout << it->emp_record.eid << " " << it->emp_record.ename << " " << it->emp_record.age << " " << it->emp_record.salary << endl;
-            ++it;
-        }
-
         // Write sorted records to temporary files (runs)
+        vector<Records>::iterator it = buffers.begin();
         runFileName = "initialRun" + to_string(tempFiles.size()) + ".csv";
         tempFiles.push_back(runFileName);
-        cout << "Writing sorted buffer to " << runFileName << "..." << endl;
         std::fstream runFile(runFileName, std::ios::out | std::ios::trunc);
-        it = buffers.begin();
         while (it != buffers.end() && it->no_values != -1) {
             if (it->emp_record.eid == -1) {
                 ++it;
@@ -78,12 +63,16 @@ int Sort_Buffer(fstream &empin) {
             }
             runFile << it->emp_record.eid << "," << it->emp_record.ename << "," << it->emp_record.age << "," << it->emp_record.salary << endl;
             ++it;
-        }
-        cout << endl << endl;
+        } // Run complete
+
+        // Increment runs
         runs++;
+        // Close run file
         runFile.close();
+        // Reset counter
         counter = 0;
 
+        // Reset buffer record objects
         for (Records& it : buffers) {
             it.emp_record.eid = -1;
             it.emp_record.ename = "";
@@ -92,20 +81,23 @@ int Sort_Buffer(fstream &empin) {
             it.no_values = 0;
         }
 
+        // Clear buffer
         if (tempRecord.no_values == -1) {
-            cout << "No more records to load..." << endl;
             buffers.clear();
             break;
         }
-       
     }
-    cout << "End of Sort_Buffer function... \nRuns: " << runs << endl << endl;
-    return runs;
+    
+    // Function complete
+    return;
 }
 
 // Utility function to merge runs and output to EmpSorted.csv
 void Merge_Runs(vector<string> tempFiles = tempFiles, int iteration = 0) {
-    //cout << "Merge runs entered... \nRuns: " << runs << endl << "Iteration: " << iteration << endl;
+  
+    // Initialize function assets
+
+    // Priority queue to hold the smallest record from each run
     std::priority_queue<Records, std::vector<Records>, CompareRecords> mainMemory;
     Records tempRecord;
     int tmpIndex;
@@ -115,164 +107,109 @@ void Merge_Runs(vector<string> tempFiles = tempFiles, int iteration = 0) {
     iteration++;
     int incrementer = 0;
 
+    // Vector to hold new temporary files, for next iteration of runs
     vector<string> newTempFiles;
 
     int runs = tempFiles.size();
     
-
+    // Go until there are no more runs to merge
     while (runs > 0) {
-        cout << "While runs > 0... Runs: " << runs << endl;
-        cout << "Size of tempfiles vector: " << tempFiles.size() << endl;
         // Open streams for as many runs as possible: up to 21 in this case
         vector<std::fstream> runFiles(BUFFER_SIZE - 1);
 
+        // Determine how many runs to merge in this iteration, up to buffer max
         if (runs > BUFFER_SIZE - 1) {
             loopIndex = BUFFER_SIZE - 1;
-            cout << "Runs greater than buffer size... Loop index: " << loopIndex << endl;
         } else {
             loopIndex = runs;
-            cout << "Runs less than buffer size... Loop index: " << loopIndex << endl;
         }
 
         // Populate the priority queue with the first record from each run
-        cout << "Populating priority queue..." << endl;
         for (int i = 0; i < loopIndex; i++) {
-            cout << "Value of i: " << i << endl;
-            cout << "Opening file: " << tempFiles.at(i) << "..." << endl;
+            // Open run file
             runFiles.at(i).open(tempFiles.at(i + incrementer), std::ios::in);
-            cout << "File opened..." << endl;
+            // Load first record from run file
             tempRecord = Grab_Emp_Record(runFiles.at(i));
-            cout << "Record grabbed..." << endl;
-
+         
+            // EOF reached for run file, skip
             if (tempRecord.no_values == -1) {
                 continue;
             }
+            // Set file stream index
             tempRecord.fileStreamIndex = i;
+            // Add record to priority queue
             mainMemory.push(tempRecord);
-            //cout << "Printing temp record..." << endl;
-            tempRecord = mainMemory.top();
-            //tempRecord.printRecord();
         }
 
+        // Used to increment through tempFiles, specifically
         incrementer += loopIndex;
 
-        //tempFiles.clear(); // Clear temp files for next iteration
-
-        std::priority_queue<Records, std::vector<Records>, CompareRecords> tempQueue = mainMemory; // Copy the original queue
-        //cout << endl << "Printing priority queue contents..." << endl;
-        /*
-        while (!tempQueue.empty()) {
-            Records record = tempQueue.top();
-            record.printRecord(); // Process the top element
-            tempQueue.pop(); // Remove the top element
-        }
-        */
-
-        // We now have a priority queue with the first record from each run loaded
-        
-        // If there are guaranteed to be more than one output file for this iteration, open as run
+        // We now have a priority queue with the first record from each run loaded into main memory
+        // If there are guaranteed to be more than one output file for this iteration, open as one of multiple runs
         if (runs > BUFFER_SIZE - 1 || newRuns > 0 ) {
-            //cout << "Writing to newRun_" << iteration << "_" << newRuns << ".csv..." << endl;
             string newFileName = "newRun_" + to_string(iteration) + "_" + to_string(newRuns) + ".csv";
-            cout << "Opening new run file " << newFileName << " to add to next iterations tempFiles." << endl;
             newRunFile.open(newFileName, std::ios::out | std::ios::trunc);
             newTempFiles.push_back(newFileName);
             newRuns++;
         } 
         // Otherwise, write to EmpSorted.csv
         else {
-            //cout << "Writing to EmpSorted.csv..." << endl;
             newRunFile.open("EmpSorted.csv", std::ios::out | std::ios::trunc);
         }
 
+        // Check if file opened
         if (!newRunFile.is_open()) {
-            //cout << "Error opening new run file..." << endl;
-            exit(1);
+            cerr << "Error: Could not open file" << endl;
+            exit(-1);
         }
         
-        // Pop the smallest record from the priority queue and write it to new run file
+        // Write records to new run file as long as there are records in main memory
         while (!mainMemory.empty()) {
             
-            // Get smallest record
+            // Retrieve smallest record
             tempRecord = mainMemory.top();
-            // Remove smallest record from priority queue
+            // Then emove smallest record from priority queue
             mainMemory.pop();
 
             // Write smallest record to new run file
-            //cout << "Writing record " << tempRecord.emp_record.eid << " to new run file: " << endl;
-
-            // cout << tempRecord.emp_record.eid << " " << tempRecord.emp_record.ename << " " << tempRecord.emp_record.age << " " << tempRecord.emp_record.salary << endl;
             newRunFile << tempRecord.emp_record.eid << "," << tempRecord.emp_record.ename << "," << tempRecord.emp_record.age << "," << tempRecord.emp_record.salary << endl;
 
             // Replace smallest record in priority queue with next record from the same run
-            //cout << "Grabbing next record from run file " << tempRecord.fileStreamIndex << "..." << endl;
-            //cout << "Record for " << tempRecord.emp_record.eid << " replaced by record for ";
-
+            // Make sure to correctly associate the new record with the correct file stream
             tmpIndex = tempRecord.fileStreamIndex;
             tempRecord = Grab_Emp_Record(runFiles.at(tempRecord.fileStreamIndex));
             tempRecord.fileStreamIndex = tmpIndex;
 
             // EOF reached for run file
             if (tempRecord.no_values == -1) {
-                //cout << "No more records in run file " << tempRecord.fileStreamIndex << "..." << endl;
                 continue;
             } else {
                 // Add replacement record to main memory
                 mainMemory.push(tempRecord);
-                }
-
-            // Finish print out statement above
-            cout << tempRecord.emp_record.eid << endl;
+            }
         }
-        //cout << "Merger loop complete. Main memory and associated runs exhausted." << endl;
-
-        
+        // Decrement total runs by the number processed this iteration through the loop
         runs -= loopIndex;
-
-        if (runs > 0) {
-            cout << "Runs remaining: " << runs << endl;
-        }
-
-        // Stop measure for testing
-        /*
-        if (iteration == 2) {
-            exit(1);
-        }
-        */
-
+        // Close run file currently being written to
         newRunFile.close();
         
     } // Ends when all runs for current iteration have been merged
 
+    // Delete temporary files from completed iteration
+    for (string& file : tempFiles) {
+        remove(file.c_str());
+    }
+
+    // If a new iteration of run was produced, call Merge_Runs recursively on new set of runs and new set of tempFile names
     if (newRuns > 0) {
             newRunFile.close();
-            cout << "Recursive call to merge run. New runs: " << newRuns << " Iteration: " << iteration << endl;
             Merge_Runs(newTempFiles, iteration);  // Recursively merge runs until all runs are merged
-        }
-    else {
-        cout << "All runs merged. Exiting merge run function..." << endl;
-    }
-        
-}
-
-void PrintSorted() {
-    // This could be integrated within Merge_Runs or called after merging
-    // to finalize writing to EmpSorted.csv
+        }  
 }
 
 int main() {
     fstream empin, SortOut;
-    //int runs = 0;
-    cout << "Begin sorting..." << endl;
     empin.open("Emp.csv", ios::in);
-    cout << "File opened..." << endl;
-    // SortOut.open("EmpSorted.csv", ios::out | ios::app);
-    cout << "Output file opened..." << endl;
-
-    int runs = Sort_Buffer(empin); // Create sorted runs
+    Sort_Buffer(empin); // Create sorted runs
     Merge_Runs(tempFiles);  // Merge runs and print sorted data
-
-    // Clean-up code to delete temporary files
 }
-
-
